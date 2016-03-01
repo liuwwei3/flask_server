e81187602086c9fc00efeb93af58d62098c286f9
liuwwei3 <liuwwei3@gmail.com>
2016-03-01 15:37:24 +0800

    del something

--- Forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form 
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
-
-class LoginForm(Form):
-
-	remeber_me = BooleanField('RemeberMe', default = False)
